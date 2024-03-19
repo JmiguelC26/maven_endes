859d7de21e3408690959770c65bd03224d1fc015
miguel <jantsag774@g.educaand.es>
2024-03-19 20:45:30 +0100

    uso de faker para datos

--- mi-aplicacion/src/main/java/empleado/EmployeeManager.java
+++ mi-aplicacion/src/main/java/empleado/EmployeeManager.java
@@ -1,32 +1,40 @@
 package empleado;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.github.javafaker.Faker;
 public class EmployeeManager {
 	
-	private List<Employee> employees;
+private List<Employee> employees;
 	
-	public EmployeeManager() {
+public EmployeeManager() {
         this.employees = new ArrayList<>();
     }
 	
-	public void addEmployee(String name, int years) {
-        Employee employee = new Employee(name, years);
-        employees.add(employee);
+public void addEmployee(String name, int years) {
+        Employee e1 = new Employee(name, years);
+        employees.add(e1);
         System.out.println(name + " added to the system.");
-    }
+}
 	
-	public void printEmployees() {
+public void printEmployees() {
         System.out.println("List of employees:");
-        for (Employee employee : employees) {
-            System.out.println(employee.getName() + ", Years in company: " +
-                    employee.getYearsInCompany());
+        for (Employee e : employees) {
+            System.out.println(e.getName() + ", Years in company: " +
+                    e.getYearsInCompany());
         }
-    }
+}
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
+	int total=100;
+	Faker faker = new Faker();
+	
         EmployeeManager manager = new EmployeeManager();
         manager.addEmployee("John Doe", 5);
         manager.addEmployee("Jane Smith", 2);
         manager.printEmployees();
+        for(int i=0;i<total;i++) {
+        	manager.addEmployee(faker.name().name(), faker.number().numberBetween(3, 80));
+        }
     }
 }
